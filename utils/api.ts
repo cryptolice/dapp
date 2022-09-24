3a4b6c6ab712fcccc09ed9b92c079f20760b8279
bruceeewong <292803281@qq.com>
2022-09-24 18:16:29 -0400

    chore

--- utils/api.ts
+++ utils/api.ts
@@ -1,4 +1,6 @@
-async function addressSecurity(chain_id: string, address: string) {
+// const chain_id: string = "56";
+// const address: string = "0x312bc7eaaf93f1c60dc5afc115fccde161055fb0"
+export async function addressSecurity(chain_id: string, address: string) {
     const body = { "chain_id": chain_id, "address": address };
 
     const response = await fetch('https://www.pipsr.cloud/api/v1/address-security', {
@@ -10,7 +12,3 @@ async function addressSecurity(chain_id: string, address: string) {
     console.log(data);
     return data;
 }
-
-const chain_id: string = "56";
-const address: string = "0x312bc7eaaf93f1c60dc5afc115fccde161055fb0"
-addressSecurity(chain_id, address)
